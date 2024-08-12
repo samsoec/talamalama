c6f14e6073cb530ed1bbaf2e10f5bde61326afca
Rizal Fauzy <muhammad.rizal@ruangguru.com>
2024-08-13 05:51:47 +0700

    fix navbar panel

--- frontend/src/app/[lang]/components/Navbar.tsx
+++ frontend/src/app/[lang]/components/Navbar.tsx
@@ -113,7 +113,7 @@ export default function Navbar({
         >
           <div className="fixed inset-0 z-40 bg-gray-600 bg-opacity-75" />{" "}
           {/* Overlay */}
-          <Dialog.Panel className="fixed inset-y-0 rtl:left-0 ltr:right-0 z-50 w-full overflow-y-auto bg-gray-800 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-inset sm:ring-white/10">
+          <Dialog.Panel className="fixed inset-y-0 rtl:left-0 ltr:right-0 z-50 w-full overflow-y-auto bg-primary px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-inset sm:ring-white/10">
             <div className="flex items-center justify-between">
               <a href="#" className="-m-1.5 p-1.5">
                 <span className="sr-only">Strapi</span>
